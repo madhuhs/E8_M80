9a7c67448056765eee4942ee94420208fa4a2ea3
gaganvdeveloper <gaganvdeveloper@gmail.com>
2025-09-13 12:42:00 +0530

    adding toString()

---
+++ src/com/jspiders/object/Mainclass2.java
@@ -0,0 +1,49 @@
+package com.jspiders.object;
+
+class Demo extends Object {
+    @Override
+    public int hashCode(){
+       int v1 = 100;
+       return v1;
+    }
+
+    @Override
+    public String toString(){
+        String s1 = "hello";
+        return s1;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        return true;
+    }
+}
+
+
+public class Mainclass2 {
+
+    public static void main(String[] args) {
+        System.out.println("Program starts...");
+
+        Demo d1 = new Demo();
+        int h1 = d1.hashCode();
+        System.out.println("h1 : "+h1);
+
+        String s1 = d1.toString();
+        System.out.println("s1 : "+s1);
+
+        System.out.println(d1.toString());
+        System.out.println(d1);
+
+        System.out.println("Program ends...");
+    }
+}
+
+
+
+
+
+
+
+
+
