07765d17355f5fe9c3d6ca822e2858b5dd50ec34
madhuhs <madhuhs943@gmail.com>
2025-09-24 11:48:07 +0530

    arrays mini proj-add,get

--- src/com/jspiders/arrays/Mainclass.java
+++ src/com/jspiders/arrays/Mainclass.java
@@ -33,27 +33,66 @@ class Student {
 public class Mainclass {
     public static void main(String[] args) {
         System.out.println("Program starts...");
-        Student[] students = new Student[4];
+//        Student[] students = new Student[4];
+//
+//        Student s1 = new Student();
+//        s1.setName("S1");
+//        s1.setMarks(50);
+//        s1.setRollNo(1);
+//
+//        students[0] = s1;
+//
+//        students[1] = new Student();
+//        students[1].setName("S2");
+//        students[1].setMarks(50);
+//        students[1].setRollNo(2);
+//
+//        System.out.println(students);
+//
+//        System.out.println("Name  : "+students[0].getName());
+//        System.out.println("Marks : "+students[0].getMarks());
+//        System.out.println("Roll No :"+students[0].getRollNo());
 
+         StudentService sv = new StudentService();
         Student s1 = new Student();
         s1.setName("S1");
         s1.setMarks(50);
         s1.setRollNo(1);
 
-        students[0] = s1;
-
-        students[1] = new Student();
-        students[1].setName("S2");
-        students[1].setMarks(50);
-        students[1].setRollNo(2);
-
-        System.out.println(students);
-
-        System.out.println("Name  : "+students[0].getName());
-        System.out.println("Marks : "+students[0].getMarks());
-        System.out.println("Roll No :"+students[0].getRollNo());
+        sv.addStudent(s1);
+        sv.getStudent(100);
 
 
         System.out.println("Program ends...");
+    }
+}
+
+class StudentService
+{
+    private Student[] students = new Student[5];
+    private int currentIndex = 0;
+
+    public void addStudent(Student student){
+        if(currentIndex<=students.length-1){
+            students[currentIndex] = student;
+            currentIndex++;
+            System.out.println("Student added");
+        }
+        else
+        {
+            System.out.println("Array is full cannot add any other student");
+        }
+    }
+
+    //prints the student data in given index
+    public void getStudent(int index){
+        if(index >= 0 && index<=students.length-1){
+            System.out.println("Name  : "+students[index].getName());
+        System.out.println("Marks : "+students[index].getMarks());
+        System.out.println("Roll No :"+students[index].getRollNo());
+        }
+        else {
+            System.out.println("Invalid index");
+        }
     }
 }
